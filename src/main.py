23cc8168f595a98b5689764d8e3942e04c9619ad
Danilo Conrado <danilo@premiersoft.net>
2021-07-18 13:49:30 -0300

    add: grid layout

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication
 from mainwindow import MainWindow
 
 
-def test(test_list: []):
+def test(test_list: list):
     print(test_list)
 
 
--- src/mainwindow.py
+++ src/mainwindow.py
@@ -6,10 +6,10 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle('EzSudoku')
-        self.setFixedSize(520, 400)
 
         self._create_menu_bar()
         self._create_grid_view()
+        self.setFixedSize(250, 250)
         self._center_on_screen()
 
     def _center_on_screen(self):
@@ -22,25 +22,32 @@ class MainWindow(QMainWindow):
         menu_bar = self.menuBar()
 
         reset_action = QAction('&Reset', self)
-        about_action = QAction('&About', self)
+        # about_action = QAction('&About', self)
 
+        reset_action.setStatusTip("Start a new game")
         reset_action.triggered.connect(self._menu_action)
-        about_action.triggered.connect(self._menu_action)
+        # about_action.triggered.connect(self._menu_action)
 
         menu_bar.addAction(reset_action)
-        menu_bar.addAction(about_action)
+        # menu_bar.addAction(about_action)
 
     def _create_grid_view(self):
         widget = QWidget(self)
         grid_layout = QGridLayout()
         widget.setLayout(grid_layout)
         self.setCentralWidget(widget)
-
+        self.buttons = list([])
         button: QPushButton
         for i in range(3):
             for j in range(3):
-                button = QPushButton(str(3 * i + j))
+                button = QPushButton()
+                button.setFixedSize(50, 50)
+                self.buttons.append(button)
                 grid_layout.addWidget(button, i, j)
+        self.status_bar = QStatusBar()
+        self.setStatusBar(self.status_bar)
+        self.status_label = QLabel("Game in progress...")
+        self.status_bar.addPermanentWidget(self.status_label)
 
     def _menu_action(self, checked):
         action = self.sender().text()
