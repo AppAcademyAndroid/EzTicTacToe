ed6c8aef6b7f165c67fb34fabf6b367ace838c9d
Danilo Conrado <danilo@premiersoft.net>
2021-07-19 09:34:05 -0300

    add: tictactoe board

--- src/main.py
+++ src/main.py
@@ -3,11 +3,20 @@ from PyQt5.QtWidgets import QApplication
 from mainwindow import MainWindow
 
 
-def test(test_list: list):
-    print(test_list)
+main_board = [[0] * 3] * 3
+
+
+def print_board(board: [[]]):
+    for i in range(3):
+        for j in range(3):
+            if j == 2:
+                print(board[i][j])
+            else:
+                print(f"{board[i][j]} ", end="")
 
 
 def main():
+    print_board(main_board)
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
