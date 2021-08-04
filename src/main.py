61898ad92d9756727c217cf6c0568b0597edb8a6
Danilo Conrado <danilo@premiersoft.net>
2021-08-04 20:09:37 -0300

    add: min max

--- src/main.py
+++ src/main.py
@@ -7,25 +7,33 @@ window: MainWindow
 main_board = [[0] * 3 for _ in range(3)]
 
 
-# -1 = game in progress
-# 0 = tie
-# 1 = X won
-# 2 = Y won
-def game_status(board: [[]]) -> int:
+# Not played = 0
+# Player X = 1
+# Player O = 2
+def evaluate_board(board: [[]], player: int) -> int:
     for i in range(3):
-        if board[i][0] == 0:
-            continue
-        if board[i][0] == board[i][1] and board[i][0] == board[i][2]:
-            return board[i][0]
+        first_cell = board[i][0]
+        if first_cell != 0 and first_cell == board[i][1] and first_cell == board[i][2]:
+            return 10 if first_cell == player else -10
+
     for i in range(3):
-        if board[0][i] == 0:
-            continue
-        if board[0][i] == board[1][i] and board[0][i] == board[2][i]:
-            return board[0][i]
-    mid = board[1][1]
-    if mid != 0 and ((mid == board[0][0] and mid == board[2][2]) or (mid == board[0][2] and mid == board[2][0])):
-        return board[1][1]
+        first_cell = board[0][i]
+        if first_cell != 0 and first_cell == board[1][i] and first_cell == board[2][i]:
+            return 10 if first_cell == player else -10
+
+    middle_cell = board[1][1]
+    if middle_cell != 0 and ((middle_cell == board[0][0] and middle_cell == board[2][2]) or (middle_cell == board[0][2] and middle_cell == board[2][0])):
+        return 10 if middle_cell == player else -10
+
     return 0
+
+
+def has_moves_left(board: [[]]) -> bool:
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                return True
+    return False
 
 
 def print_board(board: [[]]):
@@ -35,6 +43,27 @@ def print_board(board: [[]]):
                 print(board[i][j])
             else:
                 print(f"{board[i][j]} ", end="")
+
+
+def min_max(board: [[]], depth: int, is_max: bool) -> int:
+    player = 1 if is_max else 2
+    score = evaluate_board(board, player)
+
+    if score != 0:
+        return score
+
+    best_score = -1000 if is_max else 1000
+    min_max_result: int
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                board[i][j] = player
+                min_max_result = min_max(board, depth + 1, not is_max)
+                best_score = max(best_score, min_max_result) if is_max else min(best_score, min_max_result)
+                board[i][j] = 0
+
+    return best_score
 
 
 def on_action(x: int, y: int):
